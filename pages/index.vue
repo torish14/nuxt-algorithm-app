bb0452b2c23ba9af1ebfdafd6bcf8e3dfb606c00
toshihirokato <nanamin.104@icloud.com>
2021-04-08 13:23:58 +0900

    メインページの作成

---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-container>
+    <v-row column justify-center align-center>
+      <v-col xs12 sm8 md6>
+        <v-img src="img/front_image.png" max-width="400px"></v-img>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+
+@Component
+export default class Index extends Vue {}
+</script>
+
+<style></style>
