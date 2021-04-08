5c1b2cfb05c2952f7c549b89eabb7624907695fc
toshihirokato <nanamin.104@icloud.com>
2021-04-08 13:38:39 +0900

    チケットタイトルの修正

--- modules/search/search_util.ts
+++ modules/search/search_util.ts
@@ -3,7 +3,7 @@ const contents = [
   'サカナクション NF in MIDNIGHT SONIC 2019/08/16',
   '山下達郎 全国ツアー 2019/08/07',
   '宇多田ヒカル Laughter in the dark 2018/11/07',
-  '椎名林檎 （生） 林檎博 2018/11/25',
+  '椎名林檎 （生）林檎博 2018/11/25',
 ]
 
 const ticketGenerator = (num: any) => {
