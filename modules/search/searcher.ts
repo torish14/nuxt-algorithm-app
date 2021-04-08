e2fed9319cef6ac09236ce4ee25b898eab97a877
toshihirokato <nanamin.104@icloud.com>
2021-04-08 12:01:12 +0900

    線形探索と二分探索のロジック

---
+++ modules/search/searcher.ts
@@ -0,0 +1,38 @@
+export default class Searcher {
+  // 線形探索
+  static linearSearch(tickets: any, target: any) {
+    const result = { count: 1, ticket: null }
+    for (const ticket of tickets) {
+      if (ticket.id === target) {
+        return result
+      }
+      result.count++
+    }
+    return null
+  }
+
+  // 二分探索
+  static binarySearch(tickets: string | any[], target: number) {
+    if (tickets.length === 0) {
+      return null
+    }
+    const result = { count: 1, ticket: null }
+
+    let left = 0
+    let right = tickets.length - 1
+    while (left <= right) {
+      const mid = Math.floor((left + right) / 2)
+
+      if (tickets[mid].id === target) {
+        result.ticket = tickets[mid]
+        return result
+      } else if (tickets[mid].id < target) {
+        left = mid + 1
+      } else {
+        right = mid - 1
+      }
+      result.count++
+    }
+    return null
+  }
+}
